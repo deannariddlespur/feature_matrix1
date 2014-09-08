e101fe5c863fa4de657cb2f0ca4557b3a0b147f4
webdevdea <webdevdea@gmail.com>
2014-09-08 11:54:37 -0400

    Adding Some Color

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -14,11 +14,34 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<style type="text/css">
+caption {
+  background: #CCFF00;
+  font-weight: bold;
+  font-size: 3.3em;
+}
+	.list-group{
+		width: 200px;
+	}
+    .bs-example{
+    	margin: 20px;
+    	
+    }
+    </style>
+    
 
 </head>
-	<body style = "padding-top: 250px;">
+	<body style = "padding-top: 150px; background-color: #CCFF00;
+	color: #990066;
+	
+
+</style>">
 	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-				  <div class="jumbotron">
+				  <div class="wide">
 		     {{HTML::image('images/DynamiX-logo.png')}}
 	      </div> 	
       <!-- Static navbar -->
@@ -32,9 +55,11 @@
               <span class="icon-bar"></span>
             </button>
           </div>
+ 
           <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-              <li class="active"><a href="#">Home</a></li>
+            
+              <li class="active">  <a href="#">Home</a></li>
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Features<span class="caret"></span></a>
                 <ul class="dropdown-menu" role="menu">
@@ -46,9 +71,11 @@
             </ul>
       <form class="navbar-form navbar-right" role="search">
         <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
+          <input type="text" class="form-control" placeholder="">
         </div>
-        <button type="submit" class="btn btn-default">Submit</button>
+      <button type="button">Search
+ 		 <span class="glyphicon glyphicon-search"></span>
+ 	  </button> 
       </form>
           </div><!--/.nav-collapse -->
         </div><!--/.container-fluid -->
